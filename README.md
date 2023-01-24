95eec4fd6b6e93d886ef69b7d4d863347831df38
falowo gbolahan <falowogbolahan@gmail.com>
2023-01-24 15:27:03 +0000

    first_dag:refactored implementation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sp500_ETL_Pipeline
+run  mkdir ./dags ./logs ./plugins ./tmp within file directory
--- dags/first_dag.py
+++ dags/first_dag.py
@@ -1,53 +1,44 @@
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.decorators import dag, task
 from datetime import datetime, timedelta
-from etl_operation_functions import transform_stock_data,load_data_into_db,extract_sp500_data,retrieve_data_from_db
+from etl_operation_functions import (
+    transform_stock_data,
+    load_data_into_db,
+    extract_sp500_data,
+    retrieve_data_from_db,
+)
 
 
 default_args = {
-    'owner':'me',
-    'start_date':datetime.now(),
-    # 'end_date':datetime(2023,1,19),
-    'email':['rushboy2000@yahoo.com'],
-    'email_on_failure':False,
-    'email_on_retry':True,
-    'depends_on_past':False,
-    'retries':2,
-    'retry_delay':timedelta(minutes=1),
+    "owner": "me",
+    "start_date": datetime.now(),
+    "email": ["rushboy2000@yahoo.com"],
+    "email_on_failure": False,
+    "email_on_retry": True,
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=1),
 }
 
-# create DAG
-with DAG(
-    'sp500_etl_pipeline_v0',
+
+@dag(
+    "sp500_etl_pipeline_v_9",
+    schedule_interval="@daily",
     default_args=default_args,
-    description='',
-    start_date=datetime.now(),
-    # continue to run dag once per day
-    schedule_interval = '@daily') as dag:
-    
-    extract_task = PythonOperator(
-        task_id='extract_sp500_data',
-        python_callable=extract_sp500_data,
-        )
+    catchup=False,
+)
+def taskflow():
+    @task
+    def get_stock_data():
+        return extract_sp500_data()
 
-    transform_task = PythonOperator(
-        task_id='transform_sp500_data',
-        python_callable=transform_stock_data,
-        # op_kwargs={"df":extract_sp500_data}
-        )
+    @task
+    def transformation(df):
+        return transform_stock_data(df)
 
-    load_task = PythonOperator(
-        task_id='load_data_into_db',
-        python_callable=load_data_into_db,
-    
-        )
-    query_database = PythonOperator(
-        task_id='query_database',
-        python_callable=retrieve_data_from_db,
-        )
+    transformation(get_stock_data())
 
-# setting up dependencies
-extract_task >> transform_task >> load_task>>query_database
-# transform_task>>extract_sp500_data
+
+dag = taskflow()
