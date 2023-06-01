37a9d1d7eca1fcc0c29086036eeef0e73a4c90a4
falowo gbolahan <falowogbolahan@gmail.com>
2023-06-01 13:19:10 +0100

    optimizing for redundancy

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,3 +47,32 @@ resource "google_bigquery_dataset" "dataset" {
   project    = var.project
   location   = var.region
 }
+
+
+# Compute Engine Instance
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
+resource "google_compute_instance" "compute_instance" {
+  name         = "redundant-instance"  # Set the name of your instance
+  machine_type = "e2-standard-4"  # Set the machine type for your instance
+  zone         = "${var.region}-a"  # Set the zone for your instance
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20230213"  # Set the image for your instance
+    }
+  }
+
+  network_interface {
+    network = "default"  # Set the network for your instance
+    access_config {
+      // Ephemeral IP will be assigned automatically
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    # Add your custom startup script commands here
+    # Example: apt-get update && apt-get install -y apache2
+    
+  EOF
+}
