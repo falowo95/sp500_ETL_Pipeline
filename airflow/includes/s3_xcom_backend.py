14290af83f2e78512a4da5d672402c07d6f5c2eb
falowo gbolahan <falowogbolahan@gmail.com>
2023-06-02 14:28:18 +0100

    resolving pylint

--- airflow/includes/s3_xcom_backend.py
+++ airflow/includes/s3_xcom_backend.py
@@ -1,17 +1,46 @@
+import pandas as pd
 from typing import Any
 from airflow.models.xcom import BaseXCom
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
-import pandas as pd
+
 import uuid
 
 
 class S3XComBackend(BaseXCom):
+    """
+    Custom XCom backend for storing and retrieving values in S3.
+
+    The `S3XComBackend` class extends the `BaseXCom` class to provide serialization and deserialization
+    methods specific to storing and retrieving values in S3. It supports serializing and deserializing
+    pandas DataFrame objects by uploading them to and downloading them from an S3 bucket.
+
+    Attributes:
+        PREFIX (str): The prefix used for S3 keys in this XCom backend.
+        BUCKET_NAME (str): The name of the S3 bucket for storing XCom values.
+
+    Methods:
+        serialize_value(value: Any) -> Any:
+            Serializes the provided value, storing DataFrame objects in S3 if detected.
+
+        deserialize_value(result) -> Any:
+            Deserializes the provided result, retrieving DataFrame objects from S3 if necessary.
+    """
+
     PREFIX = "xcom_s3://"
     BUCKET_NAME = "sp500-bucket-xcoms"
 
     @staticmethod
-    def serialize_value(value: Any):
+    def serialize_value(value: Any) -> Any:
+        """
+        Serialize the provided value, storing DataFrame objects in S3 if detected.
+
+        Args:
+            value (Any): The value to be serialized.
+
+        Returns:
+            Any: The serialized value.
+        """
         if isinstance(value, pd.DataFrame):
             hook = S3Hook()
             key = "data_" + str(uuid.uuid4())
@@ -29,6 +58,15 @@ class S3XComBackend(BaseXCom):
 
     @staticmethod
     def deserialize_value(result) -> Any:
+        """
+        Deserialize the provided result, retrieving DataFrame objects from S3 if necessary.
+
+        Args:
+            result (Any): The result to be deserialized.
+
+        Returns:
+            Any: The deserialized result.
+        """
         result = BaseXCom.deserialize_value(result)
         if isinstance(result, str) and result.startswith(S3XComBackend.PREFIX):
             hook = S3Hook()
