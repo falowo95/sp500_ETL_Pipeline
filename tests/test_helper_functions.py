6bb964a4c62f89f41e03366f4bfe2f5bfa385bec
falowo gbolahan <falowogbolahan@gmail.com>
2023-06-02 14:19:13 +0100

    refactoring test functions

--- tests/test_helper_functions.py
+++ tests/test_helper_functions.py
@@ -1,13 +1,20 @@
+import pandas as pd
 from airflow.dags.helper_functions import (
     to_local,
     extract_sp500_data_to_csv,
 )
 
 
-import pandas as pd
 
 
-def test_to_local():
+
+def test_to_local() -> None:
+    """
+    Test function for to_local.
+
+    Raises:
+        AssertionError: If any of the test assertions fail.
+    """
     # Create a sample DataFrame
     df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
 
@@ -26,16 +33,20 @@ def test_to_local():
     path.unlink()
 
 
-def test_extract_sp500_data_to_csv():
+def test_extract_sp500_data_to_csv() -> None:
+    """
+    Test function for extract_sp500_data_to_csv.
+
+    Raises:
+        AssertionError: If any of the test assertions fail.
+    """
     # Call the extract_sp500_data_to_csv function with a file name
     file_name = "test_sp500_data"
-    df = extract_sp500_data_to_csv(file_name)
+    data_frame = extract_sp500_data_to_csv(file_name)
 
     # Check that the returned object is a pandas DataFrame
-    assert isinstance(df, pd.DataFrame)
+    assert isinstance(data_frame, pd.DataFrame)
 
     # Check that the DataFrame has at least one row and one column
-    assert df.shape[0] > 0 and df.shape[1] > 0
+    assert data_frame.shape[0] > 0 and data_frame.shape[1] > 0
 
-    # Delete the file
-    path.unlink()
