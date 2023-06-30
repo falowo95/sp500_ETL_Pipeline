94f5712176e93b583819656541071931048a9323
falowo gbolahan <falowogbolahan@gmail.com>
2023-06-30 11:51:52 +0100

    fixing conflicts

---
+++ sp500_data_manager/target/run/sp500_data_manager/models/example/sp500_dbt_model.sql
@@ -0,0 +1,44 @@
+
+  
+    
+
+    create or replace table `dataengineering-378316`.`sp_500_data`.`sp500_dbt_model`
+    
+    
+    OPTIONS()
+    as (
+      -- create table `dataengineering-378316.sp_500_data.moving_averages` as (
+-- SELECT
+--   date,
+--   symbol as ticker,
+--   twenty_day_moving as twenty_ma,
+--   two_hundred_day_moving as two_hundred_ma
+-- -- FROM `dataengineering-378316.sp_500_data.sp_500_data_table`
+-- )
+-- FROM
+-- `dataengineering-378316`.`sp_500_data`.`sp_500_data_table`
+
+
+
+-- SELECT
+--   date,
+--   symbol as ticker,
+--   twenty_day_moving as twenty_ma,
+--   two_hundred_day_moving as two_hundred_ma
+-- -- FROM `dataengineering-378316.sp_500_data.sp_500_data_table`
+-- -- )
+-- FROM
+-- `dataengineering-378316`.`sp_500_data`.`sp_500_data_table`
+
+
+
+
+  SELECT
+    date,
+    symbol AS ticker,
+    twenty_day_moving AS twenty_ma,
+    two_hundred_day_moving AS two_hundred_ma
+  FROM
+    `dataengineering-378316`.`sp_500_data`.`sp_500_data_table`
+    );
+  
