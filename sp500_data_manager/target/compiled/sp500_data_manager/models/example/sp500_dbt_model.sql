9ce9e24e7ea75ae0f2faf441df43b8f7779ed694
falowo gbolahan <falowogbolahan@gmail.com>
2023-07-23 00:54:44 +0100

    dimension creation

---
+++ sp500_data_manager/target/compiled/sp500_data_manager/models/example/sp500_dbt_model.sql
@@ -0,0 +1,16 @@
+-- create table `dataengineering-378316.sp_500_data.moving_averages` as (
+-- SELECT
+--   date,
+--   symbol as ticker,
+
+
+
+
+
+  SELECT
+    date,
+    symbol AS ticker,
+    twenty_day_moving AS twenty_ma,
+    two_hundred_day_moving AS two_hundred_ma
+  FROM
+    `dataengineering-378316`.`sp_500_data`.`sp_500_data_table`
