f7b1dddcd5517efa1ab733405592672e1b0091f3
falowo gbolahan <falowogbolahan@gmail.com>
2024-11-19 03:13:17 +0000

    Remove and ignore Google credentials file

--- sp500_data_manager/models/example/dim_bollinger.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-    symbol AS ticker,
-    bollinger_up AS bolinger_up,
-    bollinger_down AS bolinger_down
-FROM
-    {{ source('sp500_dbt_source', 'sp_500_data_table') }}
